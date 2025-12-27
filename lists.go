14303556a54c036e8fdaaaa3d22f80070c95b4c9
dkds <dkds@users.noreply.github.com>
2025-12-27 22:12:21 +0530

    Test array creation and random access

--- lists.go
+++ lists.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var productNames [4]string = [4]string{"A book"}
+	fmt.Println(productNames)
+
+	productNames[2] = "A Carpet"
 
 	prices := [4]float64{11.99, 9.99, 49.99, 19.99}
 	fmt.Println(prices)
