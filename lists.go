fc65b887a6065dfa913d5440285ed25891b5f4f4
dkds <dkds@users.noreply.github.com>
2025-12-27 22:37:12 +0530

    Use sliceson arrays and slices on slices

--- lists.go
+++ lists.go
@@ -8,6 +8,34 @@ func main() {
 
 	productNames[2] = "A Carpet"
 
-	prices := [4]float64{11.99, 9.99, 49.99, 19.99}
+	prices := [4]float64{1, 2, 3, 4}
 	fmt.Println(prices)
+
+	featuredPrices := prices[1:3]
+	fmt.Println(featuredPrices)
+
+	featuredPrices = prices[:3]
+	fmt.Println(featuredPrices)
+
+	featuredPrices = prices[1:]
+	fmt.Println(featuredPrices)
+
+	featuredPrices[0] = 199.99
+	fmt.Println(featuredPrices)
+	fmt.Println(prices)
+
+	pricesLength := len(prices)
+	featuredPricesLength := len(featuredPrices)
+	fmt.Println(pricesLength)
+	fmt.Println(featuredPricesLength)
+
+	pricesCapacity := cap(prices)
+	featuredPricesCapacity := cap(featuredPrices)
+	fmt.Println("Prices: ", prices, pricesLength, pricesCapacity)
+	fmt.Println("Featured Prices: ", featuredPrices, featuredPricesLength, featuredPricesCapacity)
+
+	highlightedPrices := featuredPrices[:1]
+	highlightedPricesLength := len(highlightedPrices)
+	highlightedPricesCapacity := cap(highlightedPrices)
+	fmt.Println("Highlighted Prices: ", highlightedPrices, highlightedPricesLength, highlightedPricesCapacity)
 }
