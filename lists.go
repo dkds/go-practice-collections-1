e653a8fb0c37c108884d7fb63592b7236c92455d
dkds <dkds@users.noreply.github.com>
2025-12-28 21:48:29 +0530

    Practice collections

--- lists.go
+++ lists.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type product struct {
+	id    int32
+	title string
+	price float64
+}
+
 func main() {
 	var productNames [4]string = [4]string{"A book"}
 	fmt.Println(productNames)
@@ -47,4 +53,41 @@ func main() {
 	newPrices = append(newPrices, 5)
 
 	fmt.Println(newPrices)
+
+	// -------------------------------- //
+	hobbies := [3]string{"Computer Gaming", "Street Photography", "Listening to podcasts/music"}
+	fmt.Println(hobbies)
+
+	fmt.Println("First element: ", hobbies[0])
+	fmt.Println("Second and third element: ", hobbies[1:3])
+
+	slice1 := hobbies[:2]
+	slice2 := hobbies[:2]
+
+	fmt.Println("Slice 1: ", slice1)
+	fmt.Println("Slice 2: ", slice2)
+
+	slice3 := slice1[1:3]
+	fmt.Println("Slice 3: ", slice3)
+
+	// -------------------------------- //
+	goals := []string{"Learn go", "Create microservices with go"}
+	fmt.Println("Goals: ", goals)
+
+	goals[1] = "Test change"
+	goals = append(goals, "Add Go to CV")
+
+	fmt.Println("Goals: ", goals)
+
+	// -------------------------------- //
+	products := []product{
+		product{id: 1, title: "Product 1", price: 11.0},
+		product{id: 2, title: "Product 2", price: 12.0},
+	}
+
+	fmt.Println("Products: ", products)
+
+	products = append(products, product{id: 3, title: "Product 3", price: 13.0})
+
+	fmt.Println("Products: ", products)
 }
